0dafc88b448aee565f1bf1119f4ed8313d2be66a
Yacine MEHELLEB <archange34@hotmail.fr>
2023-12-02 13:52:05 +0100

    Generated getters and setters for FirstClass.

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -3,4 +3,17 @@ package firstPackage;
 public class FirstClass {
 int age = 34;
 String name = "Yacine";
+public int getAge() {
+	return age;
+}
+public void setAge(int age) {
+	this.age = age;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+
 }
