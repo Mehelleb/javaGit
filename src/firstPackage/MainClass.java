5040265e552cf4fa6a386312854e83cdb8de101f
Yacine MEHELLEB <archange34@hotmail.fr>
2023-12-02 14:14:07 +0100

    Added un object p2.

--- src/firstPackage/MainClass.java
+++ src/firstPackage/MainClass.java
@@ -5,6 +5,9 @@ public class MainClass {
 	public static void main(String[] args) {
 		FirstClass p1 = new FirstClass(34, "Yacine");
 		System.out.println(p1);
+		FirstClass p2 = new FirstClass(33, "Selia");
+		System.out.println(p2);
+
 
 	}
 
