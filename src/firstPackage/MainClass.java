6c4605d47b1d76ac4f9b109b15a70e5bf6f6ae49
Yacine MEHELLEB <archange34@hotmail.fr>
2023-12-02 14:05:16 +0100

    Created first object.

--- src/firstPackage/MainClass.java
+++ src/firstPackage/MainClass.java
@@ -3,7 +3,8 @@ package firstPackage;
 public class MainClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		FirstClass p1 = new FirstClass(34, "Yacine");
+		System.out.println(p1);
 
 	}
 
